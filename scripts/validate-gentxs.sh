e2f1e553d48818ff3133dd2b807e6f040372b4bc
Giancarlos Salas <giansalex@gmail.com>
2021-11-17 20:33:26 -0500

    Fix validate-gentx

--- scripts/validate-gentxs.sh
+++ scripts/validate-gentxs.sh
@@ -77,12 +77,16 @@ if [ "$(ls -A $GENTXS_DIR)" ]; then
                 exit 1
             fi
 
+            $DAEMON add-genesis-account $GENACC 1000000000000000$DENOM --home $DAEMON_HOME 
+
             $DAEMON add-genesis-account $RANDOM_KEY 1000000000000000$DENOM --home $DAEMON_HOME \
                 --keyring-backend test
 
             $DAEMON gentx $RANDOM_KEY 900000000000000$DENOM --home $DAEMON_HOME \
                 --keyring-backend test --chain-id $CHAIN_ID
 
+            cp $GENTX_FILE $DAEMON_HOME/config/gentx/
+            
             echo "..........Collecting gentxs......."
             $DAEMON collect-gentxs --home $DAEMON_HOME
             $DAEMON validate-genesis --home $DAEMON_HOME
