f15adabf046b981b2a18eaabc411fb2ae5993c61
Akash <8366960+khoslaventures@users.noreply.github.com>
2021-11-22 01:05:53 -0800

    clean random gentxs and add better validation

--- scripts/validate-gentxs.sh
+++ scripts/validate-gentxs.sh
@@ -1,91 +1,100 @@
 #!/bin/sh
 EVMOS_HOME="/tmp/evmosd$(date +%s)"
 RANDOM_KEY="randomevmosvalidatorkey"
-# CHAIN_ID=evmos_9000-2
-# DENOM=aphoton
-MAXBOND=50000000000000 # 500 Million PHOTON
+CHAIN_ID="evmos_9000-2"
+DENOM="aphoton"
+MAXBOND="50000000000000" # 500 Million PHOTON - TODO: check if correct
+DAEMON="./build/evmosd"
+GH_URL="https://github.com/tharsis/evmos"
+BINARY_VERSION="v0.2.0"
+GENTXS_DIR="$HOME/testnets/olympus_mons/gentxs"
 
 set -e
-echo "...........Init Evmos.............."
+echo "Cloning the Evmos repo and building $BINARY_VERSION"
 
 git clone $GH_URL
 cd evmos
-git checkout tags/v0.2.0
+git checkout tags/$BINARY_VERSION
 make build
-chmod +x ./build/evmosd
+chmod +x $DAEMON
 
-./build/evmosd keys add $RANDOM_KEY --keyring-backend test --home $EVMOS_HOME
+echo "Adding random validator key so that we can start the network ourselves"
+$DAEMON keys add $RANDOM_KEY --keyring-backend test --home "$EVMOS_HOME"
+$DAEMON init --chain-id $CHAIN_ID validator --home "$EVMOS_HOME"
 
-./build/evmosd init --chain-id $CHAIN_ID validator --home $EVMOS_HOME
+# Setting the genesis time earlier so that we can start the network in our test
+gsed -i '/genesis_time/c\   \"genesis_time\" : \"2021-03-29T00:00:00Z\",' "$EVMOS_HOME"/config/genesis.json
+# Update the various denoms in the genesis
+jq -r --arg DENOM "$DENOM" '(..|objects|select(has("denom"))).denom |= $DENOM | .app_state.staking.params.bond_denom = $DENOM | .app_state.mint.params.mint_denom = $DENOM' "$EVMOS_HOME"/config/genesis.json | sponge "$EVMOS_HOME"/config/genesis.json
+cat "$EVMOS_HOME"/config/genesis.json
 
-echo "..........Fetching genesis......."
-rm -rf $EVMOS_HOME/config/genesis.json
-curl -s $PRELAUNCH_GENESIS_URL > $EVMOS_HOME/config/genesis.json.bak
+echo "Making a genesis copy, as we are using it as a template for adding our gen txs"
+cp "$EVMOS_HOME"/config/genesis.json "$EVMOS_HOME"/config/genesis.json.bak
 
-# Loop through the GenTx Files
-GENTX_FILES=$(find $GENTXS_DIR -iname "*.json")
+# Process all the GenTx files, one at a time, so that we can detect which ones are flawed
+GENTX_FILES=$(find "$GENTXS_DIR" -iname "*.json")
 for GENTX_FILE in $GENTX_FILES
 do
-    echo "GentxFile::::"
-    echo $GENTX_FILE
-
-    echo "..........Reset genesis......."
-    rm -rf $EVMOS_HOME/config/genesis.json
-    cp $EVMOS_HOME/config/genesis.json.bak $EVMOS_HOME/config/genesis.json
-
-    # this genesis time is different from original genesis time, just for validating gentx.
-    # sed -i '/genesis_time/c\   \"genesis_time\" : \"2021-03-29T00:00:00Z\",' $EVMOS_HOME/config/genesis.json
-
-    GENACC=$(cat $GENTX_FILE | sed -n 's|.*"delegator_address":"\([^"]*\)".*|\1|p')
-    denomquery=$(jq -r '.body.messages[0].value.denom' $GENTX_FILE)
-    amountquery=$(jq -r '.body.messages[0].value.amount' $GENTX_FILE)
+    echo "Processing gentx file::"
+    echo "$GENTX_FILE"
+    
+    GENACC=$(jq -r '.body.messages[0].delegator_address' "$GENTX_FILE")
+    denomquery=$(jq -r '.body.messages[0].value.denom' "$GENTX_FILE")
+    amountquery=$(jq -r '.body.messages[0].value.amount' "$GENTX_FILE")
 
     echo $GENACC
     echo $amountquery
     echo $denomquery
 
     # only allow $DENOM tokens to be bonded
-    if [ $denomquery != $DENOM ]; then
+    if [ "$denomquery" != $DENOM ]; then
         echo "invalid denomination"
         exit 1
     fi
 
     # limit the amount that can be bonded
-    if [ $amountquery -gt $MAXBOND ]; then
+    if [ "$amountquery" -gt $MAXBOND ]; then
         echo "bonded too much: $amountquery > $MAXBOND"
         exit 1
     fi
 
-    ./build/evmosd add-genesis-account $GENACC 1000000000000000$DENOM --home $EVMOS_HOME
+    $DAEMON add-genesis-account "$GENACC" 1000000000000000$DENOM --home "$EVMOS_HOME"
 
-    ./build/evmosd add-genesis-account $RANDOM_KEY 100000000000000$DENOM --home $EVMOS_HOME \
+    $DAEMON add-genesis-account $RANDOM_KEY 100000000000000$DENOM --home "$EVMOS_HOME" \
         --keyring-backend test
 
-    ./build/evmosd gentx $RANDOM_KEY 90000000000000$DENOM --home $EVMOS_HOME \
+    $DAEMON gentx $RANDOM_KEY 90000000000000$DENOM --home "$EVMOS_HOME" \
         --keyring-backend test --chain-id $CHAIN_ID
 
-    cp $GENTX_FILE $EVMOS_HOME/config/gentx/
+    cp "$GENTX_FILE" "$EVMOS_HOME"/config/gentx/
 
-    echo "..........Collecting gentxs......."
-    ./build/evmosd collect-gentxs --home $EVMOS_HOME
-    sed -i '/persistent_peers =/c\persistent_peers = ""' $EVMOS_HOME/config/config.toml
+    echo "Collecting gentxs"
+    $DAEMON collect-gentxs --home "$EVMOS_HOME"
+    gsed -i '/persistent_peers =/c\persistent_peers = ""' "$EVMOS_HOME"/config/config.toml # TODO: Should be sed, but gsed for local mac testing
 
-    ./build/evmosd validate-genesis --home $EVMOS_HOME
+    $DAEMON validate-genesis --home "$EVMOS_HOME" || echo "validate-genesis failed on $GENTX_FILE" >> bad_gentxs.out
 
-    
-    echo "..........Clean gentxs......."
-    rm -rf $EVMOS_HOME/config/gentx/*.json
+    echo "Starting the node to get complete validation (module params, signatures, etc.)"
+    $DAEMON start --home "$EVMOS_HOME" &
 
-    # echo "..........Starting node......."
-    # ./build/evmosd start --home $EVMOS_HOME &
+    # TODO: RUN COMMAND IN BACKGROUND BUT IF THERE'S A PANIC WE CAN SEND IT TO THE FILE
 
-    # sleep 180s
+    sleep 5 # TODO: change to 5s when pushing
 
-    # echo "...checking network status.."
+    echo "Checking the status of the network"
+    $DAEMON status --node http://localhost:26657
 
-    # ./build/evmosd status --node http://localhost:26657
+    echo "Killing the daemon"
+    killall evmosd >/dev/null 2>&1
+
+    echo "Remove gentx files to process next ones"
+    rm -rf "$EVMOS_HOME"/config/gentx/*.json
+
+    echo "Reset the genesis file and the chain"
+    rm -rf "$EVMOS_HOME"/config/genesis.json
+    cp "$EVMOS_HOME"/config/genesis.json.bak "$EVMOS_HOME"/config/genesis.json
+    $DAEMON unsafe-reset-all
 done
 
-echo "...Cleaning the stuff..."
-# killall evmosd >/dev/null 2>&1
-rm -rf $EVMOS_HOME >/dev/null 2>&1
+echo "Cleaning the files"
+rm -rf "$EVMOS_HOME" >/dev/null 2>&1
