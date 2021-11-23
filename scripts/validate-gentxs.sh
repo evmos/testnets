94195df760fb3a189dcf1f6445139bb5c9b42e01
Akash <8366960+khoslaventures@users.noreply.github.com>
2021-11-22 17:23:52 -0800

    add nice print function

--- scripts/validate-gentxs.sh
+++ scripts/validate-gentxs.sh
@@ -28,8 +28,12 @@ contains() {
     fi
 }
 
+print() {
+    echo "$1" | boxes -d stone
+}
+
 create_genesis_template() {
-    echo "Adding random validator key so that we can start the network ourselves" | boxes -d stone
+    # Adding random validator key so that we can start the network ourselves
     $DAEMON keys add $RANDOM_KEY --keyring-backend test --home "$EVMOS_HOME" > /dev/null 2>&1
     $DAEMON init --chain-id $CHAIN_ID validator --home "$EVMOS_HOME" > /dev/null 2>&1
     
@@ -40,7 +44,7 @@ create_genesis_template() {
 }
 
 set -e
-echo "Cloning the Evmos repo and building $BINARY_VERSION" | boxes -d stone
+print "Cloning the Evmos repo and building $BINARY_VERSION"
 
 rm -rf evmos
 git clone $GH_URL > /dev/null 2>&1
@@ -54,7 +58,7 @@ chmod +x $DAEMON
 # cp "$EVMOS_HOME"/config/genesis.json "$EVMOS_HOME"/config/genesis.json.bak
 
 # Don't allow GenTx file names with spaces
-echo "Mark files with spaces, they are not allowed" | boxes -d stone
+print "Mark files with spaces, they are not allowed"
 find "$GENTXS_DIR" -type f -name "* *" | while read -r GENTX_FILE
 do
     echo "whitespace on $GENTX_FILE; won't process" | tee -a bad_gentxs.out
@@ -62,11 +66,10 @@ done
 
 # Process all the GenTx files, one at a time, so that we can detect which ones are flawed
 GENTX_FILES=$(find "$GENTXS_DIR" -type f -regex "[^ ]*.json")
-GENTX_FILES="/Users/akash/testnets/olympus_mons/gentx-300/evmos_node.json /Users/akash/testnets/olympus_mons/gentx-300/ArtiDS_Node.json /Users/akash/testnets/olympus_mons/gentx-300/TEOLIDER.json"
 for GENTX_FILE in $GENTX_FILES
 do
     create_genesis_template
-    echo "Processing gentx file::$GENTX_FILE" | boxes -d stone
+    print "Processing gentx file::$GENTX_FILE"
     
     GENACC=$(jq -r '.body.messages[0].delegator_address' "$GENTX_FILE")
     denomquery=$(jq -r '.body.messages[0].value.denom' "$GENTX_FILE")
@@ -86,6 +89,8 @@ do
     # limit the amount that can be bonded
     if [ $amountquery -gt $MAXBOND ]; then
         echo "bonded too much: $amountquery > $MAXBOND on $GENTX_FILE" | tee -a bad_gentxs.out
+        # remove gentxs and reset genesis
+        rm -rf "$EVMOS_HOME" >/dev/null 2>&1
         continue
     fi
     # TODO could add checks for commission rate but will be caught by evmosd start
@@ -93,6 +98,8 @@ do
     OUTPUT=$($DAEMON add-genesis-account "$GENACC" 1000000000000000$DENOM --home "$EVMOS_HOME" 2>&1 || true)
     if contains "$OUTPUT" "Error"; then
         echo "add-genesis-account failed on $GENTX_FILE" | tee -a bad_gentxs.out
+        # remove gentxs and reset genesis
+        rm -rf "$EVMOS_HOME" >/dev/null 2>&1
         continue
     fi 
 
@@ -104,34 +111,30 @@ do
 
     cp "$GENTX_FILE" "$EVMOS_HOME"/config/gentx/
 
-    echo "Collecting gentxs" | boxes -d stone
+    print "Collecting gentxs"
     OUTPUT=$($DAEMON collect-gentxs --home "$EVMOS_HOME" 2>&1 || true)
     if contains "$OUTPUT" "Error"; then
         echo "collect-gentxs failed on $GENTX_FILE" | tee -a bad_gentxs.out
         # remove gentxs and reset genesis
-        rm -rf "$EVMOS_HOME"/config/gentx/*.json
-        rm -rf "$EVMOS_HOME"/config/genesis.json
-        cp "$EVMOS_HOME"/config/genesis.json.bak "$EVMOS_HOME"/config/genesis.json
+        rm -rf "$EVMOS_HOME" >/dev/null 2>&1
         continue
     fi
     gsed -i '/persistent_peers =/c\persistent_peers = ""' "$EVMOS_HOME"/config/config.toml # TODO: Should be sed, but gsed for local mac testing
 
-    echo "Run validate-genesis on created genesis file" | boxes -d stone
+    print "Run validate-genesis on created genesis file"
     if ! $DAEMON validate-genesis --home "$EVMOS_HOME"; then 
         echo "validate-genesis failed on $GENTX_FILE" | tee -a bad_gentxs.out
         # remove gentxs and reset genesis
-        rm -rf "$EVMOS_HOME"/config/gentx/*.json
-        rm -rf "$EVMOS_HOME"/config/genesis.json
-        cp "$EVMOS_HOME"/config/genesis.json.bak "$EVMOS_HOME"/config/genesis.json
+        rm -rf "$EVMOS_HOME" >/dev/null 2>&1
         continue
     fi
 
-    echo "Starting the node to get complete validation (module params, signatures, etc.)" | boxes -d stone
+    print "Starting the node to get complete validation (module params, signatures, etc.)"
     $DAEMON start --home "$EVMOS_HOME" > "$TMPFILE" 2>&1 &
 
     sleep 3 # TODO: change to 5s when pushing
 
-    echo "Checking the status of the network" | boxes -d stone
+    print "Checking the status of the network"
     OUTPUT=$($DAEMON status --node http://localhost:26657 2>&1 || true)
     if contains "$OUTPUT" "Error"; then
         PANIC=$(grep "panic" "$TMPFILE")
@@ -139,9 +142,9 @@ do
     fi
 
     # echo $OUTPUT
-    echo "Killing the daemon" | boxes -d stone
+    print "Killing the daemon"
     killall evmosd > /dev/null 2>&1 || true
 
-    echo "Cleaning the files" | boxes -d stone
+    print "Cleaning the files"
     rm -rf "$EVMOS_HOME" >/dev/null 2>&1
 done
