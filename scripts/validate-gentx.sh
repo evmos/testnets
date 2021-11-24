4b3dc0069a5ef92cc3d667089453b6a2fd1f56f1
Akash <8366960+khoslaventures@users.noreply.github.com>
2021-11-23 22:29:03 -0800

    add changed files step

--- scripts/validate-gentx.sh
+++ scripts/validate-gentx.sh
@@ -21,7 +21,7 @@ make build > /dev/null 2>&1
 chmod +x "$DAEMON"
 
 # Get the diff between main and commit
-GENTX_FILE=$(git -C "$PROJECT_DIR" diff --name-only main HEAD -- "$GENTX_DIR")
+echo "$GENTX_FILE"
 LEN_GENTX=${#GENTX_FILE}
 
 if [ $LEN_GENTX -eq 0 ]; then
